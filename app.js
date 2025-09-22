ebc7362308c322ff0ea5e2e1c0e14582b2e69315
bertuolmateus <mgbertuol@gmail.com>
2025-09-21 23:46:53 -0300

    conexões com banco para deploy

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const session = require('express-session');
 const path = require('path');
 const bodyParser = require('body-parser');
+const PORT = process.env.PORT || 3060;
 
 const app = express();
 
@@ -23,6 +24,6 @@ const nfeRoutes = require('./routes/nfe');
 app.use('/', authRoutes);
 app.use('/nfe', nfeRoutes);
 
-app.listen(3060, () => {
+app.listen(PORT, () => {
   console.log('Servidor rodando em http://localhost:3060');
 });
